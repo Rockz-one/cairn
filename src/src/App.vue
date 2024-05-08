5ed0c708769f7f32992a7252efebebce318e344b
Barak Binaymin <barak.binyamin555@gmail.com>
2024-05-08 01:27:57 -0400

    added reference

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+      <!-- https://github.com/tholman/github-corners -->
       <a href="https://github.com/Rockz-one/cairn" target="_blank" class="github-corner">
         <svg width="80" height="80" viewBox="0 0 250 250" style="fill:#eee; color:#151513; position: absolute; top: 0; border: 0; right: 0;">
           <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
@@ -48,7 +48,7 @@ html,body{
 }
 /* END KEEP THIS */
 
-/* Github banner */
+/* Github banner https://github.com/tholman/github-corners */
 .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}
 @keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}
 @media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}
